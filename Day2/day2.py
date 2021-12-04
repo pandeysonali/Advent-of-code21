de10d7e20e8e20fce5c49391a4242007a1da72bf
Sonali Pandey <sonali.pandey@salesforce.com>
2021-12-04 01:33:28 -0800

    part2

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,6 +1,7 @@
-from main import part1
+from main import part1, part2
 
 with open('day02.txt', 'r') as f:
 	data = [x for x in f.read().split("\n")]
 
 	print("Part 1: %d" % part1(data))
+	print("Part 2: %d" % part2(data))
--- Day2/main.py
+++ Day2/main.py
@@ -12,4 +12,22 @@ def part1(values: list) -> int:
 		else:
 			print("Invalid input")
 	#Return product of hori and depth		
-	return (hori * depth)	
+	return (hori * depth)
+
+def part2(data: list) -> int:
+	pos = 0
+	depth = 0
+	aim = 0
+	for val in data:
+		line = val.split()
+		if line[0] == "forward":
+			pos += int(line[1])
+			depth += aim * int(line[1])
+		elif line[0] == "down":
+			aim += int(line[1])
+		elif line[0] == "up":
+			aim -= int(line[1])
+		else:
+			print("Invalid input")
+	#Return product of pos and depth		
+	return (pos * depth)	
