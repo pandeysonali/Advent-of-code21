ae68a5482af2d0ff8f255d361f6308d89b3fea22
Sonali Pandey <sonali.pandey@salesforce.com>
2021-12-03 17:13:49 -0800

    moved files to a folder

--- day1.py
+++
@@ -1,9 +0,0 @@
-from main import increases, window_increases
-
-file = open('day01.txt', 'r')
-data = []
-for line in file:
-    data.append(int(line))
-
-print("Part 1: %d" % increases(data))
-print("Part 2: %d" % window_increases(data))
